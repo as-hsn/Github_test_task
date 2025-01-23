c9a57137c6b0b953f1768b2a2ee20677eb4ae0e1
Hassan <hassanarif1471@gmail.com>
2025-01-23 19:05:59 +0500

    Merging file 1 in to main file Merging file 2 in to main file Merging file 3 in to main file Merging file 4 in to main file

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// This is branch 1
+// Using a for loop to print numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+// This is branch 2
+// Using a for loop to print numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
 // This is branch 3
+// Using a for loop to print numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+// This is branch 4
 // Using a for loop to print numbers from 1 to 100
 for (let i = 1; i <= 100; i++) {
   console.log(i);
